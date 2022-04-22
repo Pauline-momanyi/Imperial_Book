0d39f921b4251cd23fb68a76d43ec3ef63333bef
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-22 06:02:27 +0300

    Add functionality and events

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   };
 
-  
+  let fetchedHotels = []
   function fetchHotel(){
     fetch(
       "https://travel-advisor.p.rapidapi.com/restaurants/list?location_id=293919&restaurant_tagcategory=10591&restaurant_tagcategory_standalone=10591&currency=USD&lunit=km&limit=20&open_now=false&lang=en_US",
@@ -15,17 +15,37 @@ document.addEventListener("DOMContentLoaded", () => {
     )
       .then((response) => response.json())
       .then((response) => {
-        console.log(response.data);
-        renderHotel(response.data)
+        // console.log(response.data);
+        fetchedHotels = response.data
+        console.log(fetchedHotels);
+        renderHotel(fetchedHotels)
+
+        
+       
       })
       .catch((err) => console.error(err));
   }
-
-  fetchHotel()
   
 
+  
+  const searchBar = document.getElementById('searchBar')
+        searchBar.addEventListener('keyup', (e)=>{
+          let searchString = e.target.value.toLowerCase();
+            console.log(searchString);
+            //let filters = ['popo', 'jully']
+            const filteredCharacters = fetchedHotels.filter(character=>{
+              if (fetchedHotels.indexOf(character) == 4 || fetchedHotels.indexOf(character) == 11 || fetchedHotels.indexOf(character) == 18){
+          
+              }else{
+              return character.name.toLowerCase().includes(searchString)
+              }
+            });
+            //console.log(filteredCharacters); 
+            renderHotel(filteredCharacters);
+        })
+
     function renderHotel(hotels){
-      hotels.forEach(hotel=>{ 
+      hotels.map(hotel=>{ 
         if (hotels.indexOf(hotel) == 4 || hotels.indexOf(hotel) == 11 || hotels.indexOf(hotel) == 18){
           
         }else{
@@ -50,16 +70,52 @@ document.addEventListener("DOMContentLoaded", () => {
             <p class="location"><i class="fa fa-map-marker" aria-hidden="true"></i> Location</p>
             <p class="rating"><i class="fa fa-star" aria-hidden="true">${hotel.rating}</i></p>
             <p class=" pricing">${hotel.price}</p>
-            <a href="#" class="btn btn-success">Book Hotel</a>
+            <a href="#" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Reserve Table</a>
             <p class="contacts">${hotel.email}</p>
-            <p class="website">${hotel.website}</p>
+            <a class="website" href=${hotel.website}>${hotel.website}</a>
+            <br>            
+            <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+            <span id="like">0</span>
+            <br>
+            <i class="fa fa-thumbs-down" aria-hidden="true"></i>
+            <span id="dislike">0</span>
         </div>  
       `
+   
+
+      let like = hotelDiv.querySelector('#like')
+      let likes = Number(like.textContent)
+        hotelDiv.querySelector('.fa-thumbs-up').addEventListener('click', ()=>{
+          console.log('liked');
+          like.textContent = likes++
+          
+        })
+    
+        let dislike = hotelDiv.querySelector('#dislike')
+        let dislikes = Number(dislike.textContent)
+        hotelDiv.querySelector('.fa-thumbs-down').addEventListener('click', ()=>{
+          dislike.innerHTML = dislikes++
+          console.log('disliked');
+        })
+
+
+
       document.querySelector('.outerHotel').appendChild(hotelDiv)
+
+    
         }
 
     })
 
 
     }
+    let form = document.querySelector('.reserveform')
+    form.addEventListener('submit', ()=>{
+        let resname = document.getElementById('resname')
+        alert(`We received your reservation details ${resname.value}. You will receive an email shortly`)
+    })
+
+    fetchHotel()
+
+  
 });
